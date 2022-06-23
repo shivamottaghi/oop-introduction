7b77e4b35ec1d59677078043a0d7165c109ed166
shivamtg <shivamtg1376@gmail.com>
2022-06-23 10:35:13 +0200

    add exercises

---
+++ exercise_4_protected.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+/* EXERCISE 4
+
+Copy the code of exercise 2 to here and delete everything related to cola.
+
+TODO: Make all properties protected.
+TODO: Make all the other prints work without error without changing the beverage class.
+
+USE TYPEHINTING EVERYWHERE!
+*/
