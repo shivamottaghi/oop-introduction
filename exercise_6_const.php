e73a70fb606150c65e914e24668070617c51fc51
shivamtg <shivamtg1376@gmail.com>
2022-06-24 12:35:20 +0200

    ex 6

--- exercise_6_const.php
+++ exercise_6_const.php
@@ -16,3 +16,62 @@ TODO: Make sure that every print is on a new line.
 
 Use typehinting everywhere!
 */
+class beverage{
+    private string $color;
+    private float $price;
+    private string $temperature;
+    const barName = 'Het Vervolg';
+
+    public function __construct(string $color, float $price){
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = "cold";
+    }
+
+    public function getInfo() : void{
+        echo "This beverage is $this->temperature and $this->color and the price is &euro; $this->price. ";
+    }
+
+    public function doSomethingWithBarName(): string{
+        return self::barName;
+    }
+}
+
+class beer extends beverage{
+    private string $name;
+    private float $alcoholPercentage;
+
+    /**
+     * @param string $name
+     * @param float $alcoholPercentage
+     */
+    public function __construct(string $name, float $alcoholPercentage, string $color, float $price)
+    {
+        parent::__construct($color , $price);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAlcoholPercentage(): float
+    {
+        return $this->alcoholPercentage;
+    }
+
+    public function doSomethingElseWithBarName(){
+        echo beverage::barName;
+    }
+
+}
+$duvel = new beer('Duvel', 8.7, 'blond',3.5);
+echo $duvel->getAlcoholPercentage().'<br>';
+//echo $duvel->alcoholPercentage.'<br>';
+echo $duvel->getInfo().'<br>';
+//echo $duvel->color.'<br>';
+echo beverage::barName . '<br>';
+echo 'func in beverage: '.$duvel->doSomethingWithBarName() .'<br>';
+echo 'func in beer: ';
+$duvel->doSomethingElseWithBarName();
+echo '<br>';
