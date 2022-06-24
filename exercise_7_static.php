78afb66881276e4d7cc99201594dfd602c6fdba6
shivamtg <shivamtg1376@gmail.com>
2022-06-24 16:52:09 +0200

    ex 7

--- exercise_7_static.php
+++ exercise_7_static.php
@@ -11,3 +11,24 @@ TODO: Print the address without creating a new instant of the beverage class 2 t
 
 Use typehinting everywhere!
 */
+class beverage{
+    public string $color;
+    public float $price;
+    public string $temperature;
+    static string $address = "Melkmarkt 9, 2000 Antwerpen";
+
+    public function __construct( string $color, float $price){
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = "cold";
+    }
+
+    public function getInfo() : void{
+        echo "This beverage is $this->temperature and $this->color and the price is &euro; $this->price. ";
+    }
+    static function returnStatic(): string{
+        return self::$address;
+    }
+}
+echo beverage::$address .'<br>';
+echo beverage::returnStatic();
